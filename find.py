af16359095e00b6bb872dfa3884649f4169177cf
jklee103 <ksa671@naver.com>
2019-12-27 11:32:00 +0900

    Update find.py

--- find.py
+++ find.py
@@ -3,6 +3,7 @@ import sqlite3
 import json
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 database_filename = 'service.db'
 conn = sqlite3.connect(database_filename, check_same_thread=False)
 
@@ -34,6 +35,41 @@ def findall():
     rows = cs.fetchall()
     return Response(json.dumps({'result': [str(row) for row in rows]},
                ensure_ascii=False), mimetype='application/json; charset=utf-8')
+
+
+@app.route("/signin")
+def signin():
+    uid = request.args.get('uid')
+    cs = conn.cursor()
+    query = "SELECT * FROM user WHERE uid = ?;"
+    cs.execute(query, uid)
+    rows = cs.fetchall()
+    return jsonify(rows)
+
+
+@app.route("/signup")
+def signup():
+    uid = request.args.get('uid')
+    name = request.args.get('name')
+    age = request.args.get('age')
+    gender = request.args.get('gender')
+    loc = request.args.get('loc')
+    freg = request.args.get('freg')
+    baby = request.args.get('baby')
+    kid = request.args.get('kid')
+    chung = request.args.get('chung')
+    jung = request.args.get('jung')
+    no = request.args.get('no')
+    handi = request.args.get('handi')
+    hanbumo = request.args.get('hanbumo')
+    damunhwa = request.args.get('damunhwa')
+    lowsodek = request.args.get('lowsodek')
+
+    cs = conn.cursor()
+    query = "INSERT into user (uid, name, age, gender, loc, freg, baby, kid, chung, jung, no, handi, hanbumo, " \
+            "damunhwa, lowsodek) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?); "
+    cs.execute(query, (uid, name, age, gender, loc, freg, baby, kid, chung, jung, no, handi, hanbumo, damunhwa, lowsodek))
+    conn.commit()
 
 
 if __name__ == '__main__':
