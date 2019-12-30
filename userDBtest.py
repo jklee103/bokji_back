c7d4eaa2db119a6c46c92b02145f8e3dbbbeafe1
jklee103 <ksa671@naver.com>
2019-12-30 18:32:10 +0900

    Update userDBtest.py

--- userDBtest.py
+++ userDBtest.py
@@ -1,5 +1,5 @@
 import sqlite3
-# uid, 이름, 나이, 성별,지역, -불린 임신,출산,아동청소년, 청년, 중장년, 노년, 장애인, 한부모, 다문화, 저소득층
+# uid, 이름, 나이, 성별,지역, -불린 임신,출산,아동청소년, 청년, 중장년, 노년, 장애인, 한부모, 다문화, 저소득층, 보훈여부
 
 database_filename = 'service.db'
 #푸티랑 같은 디비인거 확인
@@ -14,7 +14,7 @@ def makedb():
     query = "CREATE TABLE user (uid integer primary key, name VARCHAR(20), age integer, " \
             "gender integer, loc VARCHAR(255), freg integer, baby integer, kid integer, " \
             "chung integer, jung integer, no integer, handi integer, hanbumo integer, damunhwa integer, " \
-            "lowsodek integer); "
+            "lowsodek integer, bohun integer); "
     cs.execute(query)
 
 
