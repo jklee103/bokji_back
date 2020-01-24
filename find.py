271f06c3c6e5aac529eca83d60fd3c354efcebf3
jklee103 <ksa671@naver.com>
2020-01-24 22:07:09 +0900

    Update find.py

--- find.py
+++ find.py
@@ -173,8 +173,8 @@ def findinq():
     query = "SELECT * FROM inq WHERE cate_mid = ? ORDER BY inqnum DESC;"
     cs.execute(query, (cate_mid,))
     rows = cs.fetchall()
-    return Response(json.dumps({'result': [jsonify(row) for row in rows]},
-               ensure_ascii=False), mimetype='application/json; charset=utf-8'), 200
+    return json.dumps({'result': [jsonify(row) for row in rows]},
+               ensure_ascii=False), 200
 
 
 @app.route("/addrating")
