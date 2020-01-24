4bc9c9fcc5252584df6a11732ef8c499071a8f08
jklee103 <ksa671@naver.com>
2020-01-24 22:18:16 +0900

    Update find.py

--- find.py
+++ find.py
@@ -173,7 +173,8 @@ def findinq():
     query = "SELECT * FROM inq WHERE cate_mid = ? ORDER BY inqnum DESC;"
     cs.execute(query, (cate_mid,))
     rows = cs.fetchall()
-    return jsonify({'result': [jsonify(row) for row in rows]}), 200
+    return Response(json.dumps({'result': [row for row in rows]},
+               ensure_ascii=False), mimetype='application/json; charset=utf-8')
 
 
 @app.route("/addrating")
@@ -184,7 +185,7 @@ def addrating():
     #add on duplicate to query
     cs.execute(query, (uid, name, rate))
     rows = cs.fetchall()
-    return Response(json.dumps({'result': [str(row) for row in rows]},
+    return Response(json.dumps({'result': str(row) for row in rows},
                ensure_ascii=False), mimetype='application/json; charset=utf-8')
 
 
